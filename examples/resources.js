2b88ffb3c08eee69d8d2380da72ae9dcde1f5341
Kevin O'Hara <kevinohara80@gmail.com>
2013-02-17 15:40:00 -0500

    lots of new examples

---
+++ examples/resources.js
@@ -0,0 +1,23 @@
+var nforce = require('../');
+var sfuser = process.env.SFUSER;
+var sfpass = process.env.SFPASS;
+
+var org = nforce.createConnection({
+  clientId: '3MVG9rFJvQRVOvk5nd6A4swCyck.4BFLnjFuASqNZmmxzpQSFWSTe6lWQxtF3L5soyVLfjV3yBKkjcePAsPzi',
+  clientSecret: '9154137956044345875',
+  redirectUri: 'http://localhost:3000/oauth/_callback'
+});
+
+org.authenticate({ username: sfuser, password: sfpass}, function(err, oauth) {
+  if(err) {
+    console.error('unable to authenticate to sfdc');
+  } else {
+    org.getResources(oauth, function(err, resp) {
+      if(err) {
+        console.error(err);
+      } else {
+        console.log(resp);
+      }
+    });
+  }
+});
