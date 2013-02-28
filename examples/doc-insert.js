4c3d06be16ed132540dc3b9f041b3ffda6075e87
Kevin O'Hara <kevinohara80@gmail.com>
2013-02-28 10:38:11 -0500

    remove unnecessary query from doc insert example

--- examples/doc-insert.js
+++ examples/doc-insert.js
@@ -4,9 +4,6 @@ var path   = require('path');
 var sfuser = process.env.SFUSER;
 var sfpass = process.env.SFPASS;
 
-
-var query = 'SELECT Id, Name FROM Document LIMIT 10';
-
 var org = nforce.createConnection({
   clientId: '3MVG9rFJvQRVOvk5nd6A4swCyck.4BFLnjFuASqNZmmxzpQSFWSTe6lWQxtF3L5soyVLfjV3yBKkjcePAsPzi',
   clientSecret: '9154137956044345875',
