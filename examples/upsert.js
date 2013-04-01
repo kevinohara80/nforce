9474e2512f5eaf4b58553fc4687d15c510beb58b
Kevin O'Hara <kevinohara80@gmail.com>
2013-04-01 17:54:31 -0400

    fixes #12

---
+++ examples/upsert.js
@@ -0,0 +1,34 @@
+var nforce = require('../');
+var sfuser = process.env.SFUSER;
+var sfpass = process.env.SFPASS;
+
+var oauth;
+
+var org = nforce.createConnection({
+  clientId: '3MVG9rFJvQRVOvk5nd6A4swCyck.4BFLnjFuASqNZmmxzpQSFWSTe6lWQxtF3L5soyVLfjV3yBKkjcePAsPzi',
+  clientSecret: '9154137956044345875',
+  redirectUri: 'http://localhost:3000/oauth/_callback',
+  mode: 'single'
+});
+
+function createContact() {
+  var account = nforce.createSObject('Account', { Name: 'Kevin Enterprises' });
+
+  account.setExternalId('Account_No__c', '232');
+
+  org.upsert(account, function(err, resp) {
+    if(err) return console.error(err);
+    console.log(resp);
+  });
+
+}
+
+org.authenticate({ username: sfuser, password: sfpass}, function(err, resp) {
+  if(err) {
+    console.error('--> unable to authenticate to sfdc');
+    console.error('--> ' + JSON.stringify(err));
+  } else {
+    console.log('--> authenticated!');
+    createContact();
+  }
+});
