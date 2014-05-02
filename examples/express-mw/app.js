96e54cc2ac4041712abc51907d9cdd4b15324372
Kevin O'Hara <kevinohara80@gmail.com>
2014-05-02 16:32:40 -0400

    updating old test for express 4

--- examples/express-mw/app.js
+++ examples/express-mw/app.js
@@ -1,7 +1,10 @@
-var express    = require('express');
-var nforce     = require('nforce');
-var app        = module.exports = express();
-var bodyParser = require('body-parser');
+var nforce       = require('../../');
+var sfuser       = process.env.SFUSER;
+var sfpass       = process.env.SFPASS;
+var express      = require('express');
+var bodyParser   = require('body-parser');
+var session      = require('express-session');
+var cookieParser = require('cookie-parser');
 
 var org = nforce.createConnection({
   clientId: '3MVG9rFJvQRVOvk5nd6A4swCyck.4BFLnjFuASqNZmmxzpQSFWSTe6lWQxtF3L5soyVLfjV3yBKkjcePAsPzi',
@@ -11,26 +14,14 @@ var org = nforce.createConnection({
 
 // Configuration
 
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.json());
-  app.use(express.urlencoded());
-  app.use(express.methodOverride());
-  app.use(express.cookieParser());
-  app.use(express.session({ secret: 'nforce testing baby' }));
-  app.use(org.expressOAuth({onSuccess: '/test/query', onError: '/oauth/error'}));
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
-});
-
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
-});
-
-app.configure('production', function(){
-  app.use(express.errorHandler()); 
-});
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(session({ secret: 'somesecret', key: 'sid' }));
+app.use(org.expressOAuth({onSuccess: '/test/query', onError: '/oauth/error'}));
 
 // Routes
 
@@ -42,13 +33,6 @@ app.get('/', function(req, res){
 
 app.get('/oauth/authorize', function(req, res){
   res.redirect(org.getAuthUri());
-});
-
-app.get('/oauth/invalidate', function(req, res) {
-  org.revokeToken({ oauth: oauth }, function(e, res) {
-    if(err) throw err;
-    else res.redirect('/');
-  });
 });
 
 app.get('/test/query', function(req, res) {
@@ -65,6 +49,5 @@ app.get('/test/query', function(req, res) {
   });
 });
 
-var httpPort = 3000;
-app.listen(httpPort);
-console.log("Express server listening on port %d in %s mode", httpPort, app.settings.env);
+app.listen(3000);
+console.log('express started');
