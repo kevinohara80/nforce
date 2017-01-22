4a7219a78efa76bf420e589bc830a69528e34076
Kessiler <kessiler@users.noreply.github.com>
2017-01-22 17:38:46 -0600

    Fix merge conflicts

--- index.js
+++ index.js
@@ -22,13 +22,6 @@ var LOGIN_URI          = 'https://login.salesforce.com/services/oauth2/token';
 var TEST_LOGIN_URI     = 'https://test.salesforce.com/services/oauth2/token';
 var ENVS               = ['sandbox', 'production'];
 var MODES              = ['multi', 'single'];
-var API_VERSIONS       = [
-  'v20.0', 'v21.0', 'v22.0', 'v23.0', 'v24.0',
-  'v25.0', 'v26.0', 'v27.0', 'v28.0', 'v29.0',
-  'v30.0', 'v31.0', 'v32.0', 'v33.0', 'v34.0',
-  'v35.0', 'v36.0', 'v37.0', 'v38.0'
-];
-
 
 var plugins = {};
 
